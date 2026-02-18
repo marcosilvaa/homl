39d6b5cbc3f503e91503732704426e71cf72d429
Marco Silva <marcosilvam159@gmail.com>
2026-02-17 22:08:01 -0300

    Add new files and update notebooks

---
+++ utils/save_fig.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+# Where to save the figures
+IMAGES_PATH = Path() / "images" / "fundamentals"
+IMAGES_PATH.mkdir(parents=True, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
